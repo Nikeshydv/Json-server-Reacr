baf63ad27090903774b2a3602f950abc99d60cf8
Nikeshydv <yadav.nikesh.604@gmail.com>
2024-06-20 15:43:44 +0530

    first react

---
+++ src/App.jsx
@@ -0,0 +1,157 @@
+// import Header from "./header";
+// import Data from "./data";
+// import Footer from "./footer";
+
+// const nm="Nikesh";
+// const emp="E123";
+// const Sal="45784";
+
+// import Student from "./student"
+
+// const Student={
+//   "name":"Rahul",
+//   "city":"Bhopal",
+//   "rollno":1221
+
+// }
+
+// const App=()=>{
+//   return(
+//     <>
+//     <h1> welcome to cybrom</h1>
+//     <Student rno={Student.rollno} nm={Student.name} ct={Student.city}   />
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+// import Employee from "./Employee.jsx";
+
+// const App=()=>{
+//   return(
+//     <>
+//   <Employee name={nm} empn={emp} sal={Sal} />
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+
+// const App=()=>{
+//   return(
+//     <>
+//     <h1> welcome to cybrom !!!!</h1>
+//     <Header/>
+//     <Middle/>
+//     <Footer/>
+    
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+// const App=()=>{
+//   return(
+//     <>
+//     <h1> This is form </h1>
+//     Name:<input type="text"/>
+//     <br/>
+//     <br/>
+//     Enter city:<input type="text"/>
+//     </>
+//   )
+// }
+
+// export default App;
+
+// const name=["ram","shaym","mohan","sohan"];
+
+
+// const App=()=>{
+//   const ans=name.map((key)=>{
+//     return(
+//       <>
+//       <h1>{key+key}</h1>
+//       </>
+//     )
+//   })
+//   return(
+//     <>
+//     <h1>welcome to cybrom bhopal</h1>
+//     {ans}
+//     </>
+//   )
+// }
+
+// export default App;
+
+// const age=[24,20,6,8,10,13];
+// const myage=age.map((key)=>{
+//   return(
+//     <>
+//     <h1>{key}</h1>
+//     </>
+//   )
+// })
+// const App=()=>{
+//   return(
+//     <>
+//     <h1> welcoe to cybrom student age</h1>
+//     {myage};
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+
+
+// const city=["bhopal","ujjain","indore","jabalpur","narmadapuram"];
+// const mycity=city.map((key)=>{
+//   return(
+//     <>
+//     <option>{key}</option>
+//     </>
+//   )
+// })
+
+// const App=()=>{
+//   return(
+//     <>
+//     <select>{mycity}</select>
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+
+// const subject=["php","orcles","java","asp","python"];
+
+// const mysub=subject.map((key)=>{
+//   return(
+//     <>
+//     <li>{key}</li>
+//     </>
+//   )
+// })
+
+// const App=()=>{
+// return(
+//   <>
+//   <ul>{mysub}</ul>
+//   </>
+// )
+// }
+
+// export default App;
+
+
