f64ea79daa7057f3539a8ce4224a0b01cc17871c
Nikesh Yadav <105097035+Nikeshydv@users.noreply.github.com>
2024-07-02 16:00:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,104 @@
 
 
-const display=()=>{
-  alert("this is alert box")
-}
+// const display=()=>{
+//   alert("this is alert box")
+// }
 
-const App=()=>{
-  return<>
-  <h1> this is event</h1>
-  <button onClick={display}>click</button>
-  </>
-}
+// const App=()=>{
+//   return<>
+//   <h1> this is event</h1>
+//   <button onClick={display}>click</button>
+//   </>
+// }
 
-export default App;
-
-
-//passing the argunmnet//
-
-const myName=(nm,ln)=>{
-  alert("my name : "+ nm +" "+ ln +" from bhoapl");
-}
-
-const App=()=>{
-  return(
-    <>
-    <button onClick={()=>{myName("Nikesh","Yadavanshi")}}> click</button>
-    </>
-  )
-}
-
-export default App;
+// export default App;
 
 
-//functuion to add the number
+// //passing the argunmnet//
 
-const myAdd=(a,b)=>{
- alert(a+b);
-}
+// const myName=(nm,ln)=>{
+//   alert("my name : "+ nm +" "+ ln +" from bhoapl");
+// }
 
-const App=()=>{
-  return(
-    <>
-    <h1> this is the add function </h1>
-    <button onClick={()=>{myAdd(23,520)}}>Click to add</button>
-    </>
-  )
-}
-export default App;
+// const App=()=>{
+//   return(
+//     <>
+//     <button onClick={()=>{myName("Nikesh","Yadavanshi")}}> click</button>
+//     </>
+//   )
+// }
 
-
+// export default App;
 
 
-//function to find out the event tyype
+// //functuion to add the number
+
+// const myAdd=(a,b)=>{
+//  alert(a+b);
+// }
+
+// const App=()=>{
+//   return(
+//     <>
+//     <h1> this is the add function </h1>
+//     <button onClick={()=>{myAdd(23,520)}}>Click to add</button>
+//     </>
+//   )
+// }
+// export default App;
 
 
-const display=(nm,b)=>{
-  alert(nm+" event type: "+ b.type)
-}
-
-const App=()=>{
-  return(
-    <>
-    <button onClick={(e)=>{display("ranu",e)}}> click</button>
-    </>
-  )
-}
-
-export default App;
 
 
-//without giving the parameter in dispaly button event
-
-const dispaly=(e)=>{
-  alert("event type :"+ e.type)
-}
+// //function to find out the event tyype
 
 
-const App=()=>{
-  return(
-    <>
-    <button onClick={dispaly}>click to find the eventy</button>
-    </>
-  )
-}
+// const display=(nm,b)=>{
+//   alert(nm+" event type: "+ b.type)
+// }
 
-export default App;
+// const App=()=>{
+//   return(
+//     <>
+//     <button onClick={(e)=>{display("ranu",e)}}> click</button>
+//     </>
+//   )
+// }
 
-const myVal=(e)=>{
- let nm=e.target.name;
- let val=e.target.value;
- let typ=e.target.type;
- alert("name :"+ nm +"value" + val +"type :" + typ +" event :"+ e.type);
-}
+// export default App;
 
 
-const App=()=>{
-  return(
-    <>
-    <button type="button" value="myBtn" name="btn1" onClick={myVal}>click</button>
-    </>
-  )
-}
+// //without giving the parameter in dispaly button event
 
-export default App;
+// const dispaly=(e)=>{
+//   alert("event type :"+ e.type)
+// }
+
+ 
+// const App=()=>{
+//   return(
+//     <>
+//     <button onClick={dispaly}>click to find the eventy</button>
+//     </>
+//   )
+// }
+
+// export default App;
+
+// const myVal=(e)=>{
+//  let nm=e.target.name;
+//  let val=e.target.value;
+//  let typ=e.target.type;
+//  alert("name :"+ nm +"value" + val +"type :" + typ +" event :"+ e.type);
+// }
+
+
+// const App=()=>{
+//   return(
+//     <>
+//     <button type="button" value="myBtn" name="btn1" onClick={myVal}>click</button>
+//     </>
+//   )
+// }
+
+// export default App;
